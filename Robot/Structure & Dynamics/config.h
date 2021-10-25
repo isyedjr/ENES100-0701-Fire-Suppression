d7df08b530617902f071f27f370c524d682ddeef
Imaad Syed <isyed1@terpmail.umd.edu>
2021-10-25 15:32:16 -0400

    Update config.h

--- Robot/Structure & Dynamics/config.h
+++ Robot/Structure & Dynamics/config.h
@@ -1,4 +1,34 @@
 /* Define motor pins */
-#define RIGHT1
-#define 
+#define RF 2
+#define RB 3
+#define LF 4
+#define LB 5
+#define enR 22
+#define enL 24
 
+/* Nav pins */
+#define S1Trig 6
+#define S1Echo 7
+#define S2Trig 8 
+#define S2Echo 9
+
+/* Fire detection */
+#define pr1 A0
+#define pr2 A1
+#define pr3 A2
+#define pr4 A3
+#define tr1 A4
+#define tr2 A5
+#define tr3 A6
+#define tr4 A7
+
+/* Servo */
+#define s1 10
+#define s2 11
+#define s3 12
+
+/* Stepper */
+#define m1 26
+#define m2 28
+#define m3 30
+#define m4 32
